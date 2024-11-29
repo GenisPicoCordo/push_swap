a161170c92eb68fedc964b1410fc7e98e2559165
Genís Picó Cordo <genis.pico@hotmail.com>
2024-11-29 11:01:55 +0100

    Update main.c

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -1,22 +1,25 @@
 #include "push_swap.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
+
     t_stack *a;
     int i;
-    
+
+    a = init_stack();
+    i = 1;
     if (argc < 2)
         return (0);
     if (!validate_arguments(argc, argv))
         return (1);
-    
-    a = init_stack();
-    i = 1;
-    while (i < argc) {
+    while (i < argc)
+    {
         push(a, atoi(argv[i]));
         i++;
     }
+
     // Aquí implementaremos el algoritmo de ordenamiento
+
     free_stack(a);
     return (0);
 }
-
