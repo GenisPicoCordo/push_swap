c7022218bec385051b43f9a6140fcd5d6780953b
Genís Picó Cordo <genis.pico@hotmail.com>
2024-12-03 22:31:35 +0100

    Update push_swap.c

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -18,65 +18,29 @@ int	main(int argc, char **argv)
 	t_stack	*b;
 	int		i;
 
-	a = init_stack();
-	b = init_stack();
-	i = 1;
-
 	if (argc < 2)
 		return (0);
 	if (!validate_arguments(argc, argv))
-	{
-		write(2, "Error\n", 6);
 		return (1);
-	}
-
+	a = init_stack();
+	b = init_stack();
+	if (!a || !b)
+		return (1);
+	i = 1;
 	while (i < argc)
 	{
 		push_bottom(a, ft_atoi(argv[i]));
 		i++;
 	}
-
-	sort_five(a, b);
-
+	if (a->size == 2)
+		sort_two(a);
+	else if (a->size == 3)
+		sort_three(a);
+	else if (a->size == 4 || a->size == 5)
+		sort_five(a, b);
+	else
+		//
 	free_stack(a);
 	free_stack(b);
-
 	return (0);
 }
-
-/*
-#include "push_swap.h"
-#include <stdio.h>
-
-int main() {
-    t_stack *a = init_stack();
-    t_stack *b = init_stack();
-
-    // Agregar elementos desordenados
-    push_bottom(a, 3);
-    push_bottom(a, 1);
-    push_bottom(a, 4);
-    push_bottom(a, 5);
-    push_bottom(a, 2);
-
-    printf("Antes de sort_five:\n");
-    t_node *current = a->top;
-    while (current) {
-        printf("%d\n", current->value);
-        current = current->next;
-    }
-
-    sort_five(a, b);
-
-    printf("Después de sort_five:\n");
-    current = a->top;
-    while (current) {
-        printf("%d\n", current->value);
-        current = current->next;
-    }
-
-    free_stack(a);
-    free_stack(b);
-    return (0);
-}
-*/
